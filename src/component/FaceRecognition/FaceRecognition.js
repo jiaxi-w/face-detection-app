5f752b86227407ef34f683bca1815716224ff297
Jiaxi <dev.jiaxiwu@gmail.com>
2020-06-18 15:52:13 -0400

    photo edited

--- src/component/FaceRecognition/FaceRecognition.js
+++ src/component/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className="center">
-      <img src={imageUrl} alt="pic"></img>
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img src={imageUrl} alt="pic" width="500px" height="auto"></img>
+      </div>
     </div>
   );
 };
