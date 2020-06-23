ed9e96a20414c07c05471ebd2523f25ce608bb1c
Jiaxi <dev.jiaxiwu@gmail.com>
2020-06-22 20:01:16 -0400

    js

--- src/App.js
+++ src/App.js
@@ -31,8 +31,15 @@ class App extends Component {
     this.state = {
       input: " ",
       imageUrl: " ",
+      box: {},
     };
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -41,16 +48,11 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
 
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function (err) {
-        // there was an error
-      }
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.calculateFaceLocation(response).catch((err) => console.log(err))
+      );
   };
 
   render() {
